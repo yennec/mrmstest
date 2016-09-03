1bc6ac3afeff72f23a69b25cc89b32e24ee568cd
mskim <pri.mskim@gmail.com>
2016-09-03 23:56:09 +0900

    1st commit

---
+++ mrms_inner/src/hellomrmsinner.scala
@@ -0,0 +1,6 @@
+/**
+  * Created by mskim on 2016. 9. 3..
+  */
+object hellomrmsinner extends App {
+  println("Hello, MR.MS Inner!")
+}
---
+++ src/hellomrms.scala
@@ -0,0 +1,6 @@
+/**
+  * Created by mskim on 2016. 9. 3..
+  */
+object hellomrms extends App {
+    println("Hello, MR.MS!")
+}
